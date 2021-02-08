c774b5346841187b9c9ff586d36f9206e0a78c7e
yvette-Ye <49329430+yvette-Ye@users.noreply.github.com>
2021-02-08 12:51:23 +0800

    Delete petalService directory

--- petalService/src/main/java/com/petal/demo/repository/UserinfoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.petal.demo.repository;
-
-import com.petal.demo.entity.Userinfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserinfoRepository extends JpaRepository<Userinfo, String> {
-}
